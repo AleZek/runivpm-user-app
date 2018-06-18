fee7e8a702a71025f1d71f0554b2ee66739b9505
Ilaria Lauriola <ilalauriola@gmail.com>
2018-06-18 16:19:20 +0200

    home

--- app/src/main/java/com/ids/idsuserapp/HomeActivity.java
+++ app/src/main/java/com/ids/idsuserapp/HomeActivity.java
@@ -1,13 +1,43 @@
 package com.ids.idsuserapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.ids.idsuserapp.fragment.MapNavigationFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        button = findViewById(R.id.scegli_da_mappa_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, MapNavigationActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
-}
+
+
+
+
+
+    }
+
+
+
+
+
+
+
--- app/src/main/java/com/ids/idsuserapp/MapNavigationActivity.java
+++ app/src/main/java/com/ids/idsuserapp/MapNavigationActivity.java
@@ -1,13 +1,44 @@
 package com.ids.idsuserapp;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MapNavigationActivity extends AppCompatActivity {
+import com.ids.idsuserapp.fragment.MapNavigationFragment;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class MapNavigationActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_navigation);
+
+                    /*
+                    Fragment fr = new FirstFragment();
+fr.setArguments(args);
+FragmentManager fm = getFragmentManager();
+FragmentTransaction fragmentTransaction = fm.beginTransaction();
+fragmentTransaction.replace(R.id.fragment_place, fr);
+fragmentTransaction.commit();
+                     */
     }
 }
+
+
+
+
+  /*  private void setupMapNavigationFragment() {
+        MapNavigationFragment fragment = new MapNavigationFragment();
+        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.mapnavigationcontainer, fragment).commit();
+    }*/
+
+
