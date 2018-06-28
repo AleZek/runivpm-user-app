da8caf4939facc7261abbe23916cfc7f8cb598b7
AleZek <zechini.alessandro@gmail.com>
2018-06-28 14:37:11 +0200

    commento a firebase

--- app/src/main/java/com/ids/idsuserapp/MainActivity.java
+++ app/src/main/java/com/ids/idsuserapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,29 +22,34 @@ public class MainActivity extends AppCompatActivity {
         subscribeTopic("emergenza");
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, HomeActivity.class);
-
-        startActivity(intent);
+//        Intent intent = new Intent(this, HomeActivity.class);
+//
+//        startActivity(intent);
+//        //segmento di codice utile all unlock automaitico
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+//                + WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+//                + WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+//                + WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
     }
 
 
 
     //questo metodo permette alla app di sottoscriversi al topic emergenza, questo permette a firebase
     // di mandare messaggi broadcast alle istanze della app
-    private void subscribeTopic(final String topic){
-        FirebaseMessaging.getInstance().subscribeToTopic(topic)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        String msg = "Sottoscrizione avvenuta a ";
-                        if (!task.isSuccessful()) {
-                            msg = "sottoscrizione fallita a ";
-                        }
-                        Log.d(TAG, msg + topic); // sono mostrati dei messaggi nel log e nella app se la sottoscrizione avviene o meno
-                        Toast.makeText(MainActivity.this, msg + topic, Toast.LENGTH_SHORT).show();
-                    }
-                });
-
-
-    }
+//    private void subscribeTopic(final String topic){
+//        FirebaseMessaging.getInstance().subscribeToTopic(topic)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        String msg = "Sottoscrizione avvenuta a ";
+//                        if (!task.isSuccessful()) {
+//                            msg = "sottoscrizione fallita a ";
+//                        }
+//                        Log.d(TAG, msg + topic); // sono mostrati dei messaggi nel log e nella app se la sottoscrizione avviene o meno
+//                        Toast.makeText(MainActivity.this, msg + topic, Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//
+//    }
 }
--- app/src/main/java/com/ids/idsuserapp/PercorsoActivity.java
+++ app/src/main/java/com/ids/idsuserapp/PercorsoActivity.java
@@ -56,6 +56,7 @@ public class PercorsoActivity extends AppCompatActivity implements  BluetoothLoc
         setDijkstra();
         percorso = dijkstra.ricerca(destinazione);
         Log.v("percorso", percorso.toString());
+
     }
 
     @Override
@@ -136,4 +137,31 @@ public class PercorsoActivity extends AppCompatActivity implements  BluetoothLoc
             nextStep();
         }
     }
+
+
+    //segmento di codice utile all unlock automaitico
+//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+//                + WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD|
+//                + WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|
+//                + WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
+
+    //questo metodo permette alla app di sottoscriversi al topic emergenza, questo permette a firebase
+    // di mandare messaggi broadcast alle istanze della app
+//    private void subscribeTopic(final String topic){
+//        FirebaseMessaging.getInstance().subscribeToTopic(topic)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        String msg = "Sottoscrizione avvenuta a ";
+//                        if (!task.isSuccessful()) {
+//                            msg = "sottoscrizione fallita a ";
+//                        }
+//                        Log.d(TAG, msg + topic); // sono mostrati dei messaggi nel log e nella app se la sottoscrizione avviene o meno
+//                        Toast.makeText(MainActivity.this, msg + topic, Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+//
+//
+//    }
 }
