5d5fa735e12c71d0e220ca0d557c8d6fac785c2b
AleZek <zechini.alessandro@gmail.com>
2018-07-12 11:40:59 +0200

    dialog uscita da home

--- app/src/main/java/com/ids/idsuserapp/HomeActivity.java
+++ app/src/main/java/com/ids/idsuserapp/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.ids.idsuserapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
@@ -221,5 +222,27 @@ public class HomeActivity extends AppCompatActivity implements DataRetriever{
               else
                 permissionsUtil.btAlert();
         }
+    }
+
+    @Override
+    public void onBackPressed(){
+        final AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+        builder.setMessage("Sei sicuro di voler uscire?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("Sì", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                finish();
+            }
+        });
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
